a1f9073ffca205d2121bb2f85ce8e61e7615327f
Szczepan Bułat <szczepan.bulat@gmail.com>
2020-03-16 13:01:59 +0100

    add endpoint for slack commands

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const YAML = require('yaml');
 const _ = require('lodash');
 
 const messageFormatter = require('./lib/messageFormatter');
+const slackParamsParser = require('./lib/slackParamsParser');
 
 // DB
 const low = require('lowdb');
@@ -14,17 +15,21 @@ const FileAsync = require('lowdb/adapters/FileAsync');
 const fastify = require('fastify')({
   logger: true
 });
+fastify.register(require('fastify-formbody'));
 
+// Post in Slack
 const postInSlack = (lunches) => {
   axios.post(process.env.SLACK_WEBHOOK_URL, { text: messageFormatter.forSlack(lunches) })
-    .then(response => console.log(response.status))
-    .catch(console.log)
+    .then(response => console.log(response.status.code))
+    .catch(response => console.log(response.status))
 
   return new Promise((resolve, reject) => {
     resolve(lunches);
   });
 };
 
+
+// Routes
 const adapter = new FileAsync('./db.json');
 low(adapter)
   .then(db => {
@@ -49,6 +54,26 @@ low(adapter)
         .write()
         .then(lunch => postInSlack([lunch]))
         .then(lunches => reply.send(lunches[0]))
+    })
+
+    fastify.post('/slack', (request, reply) => {
+      console.log("BODY", request.body);
+      const userName = request.body.user_name;
+      const parseResponse = slackParamsParser.perform(request.body.text.trim());
+      console.log(parseResponse);
+
+      if (!parseResponse.valid) {
+        return reply.send("Wrong arguments passed. Example: `/lunch-krakow 12:00 Mural Hindus`")
+      }
+
+      if (true || !params.length) {
+        // TODO: filter by today
+        const lunches = db.get('lunches').value();
+        reply.send(messageFormatter.forSlack(lunches));
+      } else {
+        // TODO: write to db (username, hour, places)
+        reply.send("NotImplementedError");
+      }
     })
 
     return db.defaults({ lunches: [] }).write();
